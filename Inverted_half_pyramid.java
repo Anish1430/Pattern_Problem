6aa374c8a21d09642fcf85ff7c3e0887b160d477
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2024-03-19 16:25:17 +0530

    Inverted half pyramid Using Nested Loop in java   using Second Method

--- Inverted_half_pyramid.java
+++ Inverted_half_pyramid.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class Inverted_half_pyramid {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-         int cols=sc.nextInt();
-         int rows=sc.nextInt();
-         for(int i=1;i<=cols;i++) {
-             for(int j=rows;j>=i;j--) {
-                 System.out.print("*");
-             }
-             System.out.println();
-         }
+         //1st Method
+//         int cols=sc.nextInt();
+//         int rows=sc.nextInt();
+//         for(int i=1;i<=cols;i++) {
+//             for(int j=rows;j>=i;j--) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+    //2nd Method in java
+        int rows=sc.nextInt();
+        for(int i=rows;i>=1;i--) {
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
