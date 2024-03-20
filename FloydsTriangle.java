552181480184f53f94b76938bee8ae166e270b12
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2024-03-20 20:07:25 +0530

    floyds triangle in java using 2nd Method

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class FloydsTriangle {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-         int n=5;
-         int num=1;
-         for(int i=n;i>=1;i--){
-             for(int j=i;j<=n;j++) {
-                 System.out.print(num+" ");
-                   num++;
-             }
-
-             System.out.println();
-         }
+//         int n=5;
+//         int num=1;
+//         for(int i=n;i>=1;i--) {
+//             for(int j=i;j<=n;j++) {
+//                 System.out.print(num+" ");
+//                   num++;
+//             }
+//             System.out.println();
+//         }
+        int n=sc.nextInt();
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                  num++;
+            }
+            System.out.println();
+        }
     }
 }
