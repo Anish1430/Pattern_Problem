bb6cfc92105bc5f25eb67b6d2f5acfa87b62d3fc
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2024-03-26 16:44:26 +0530

    K pattern and Diamind Pattern in java Mirror Pattern Triangle

--- Kpattern.java
+++ Kpattern.java
@@ -4,6 +4,8 @@ public class Kpattern {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
           int n=sc.nextInt();
+
+          //For First Half Printing.....
           for(int i=1;i<=n;i++) {
               for(int j=n;j>=i;j--){
                   System.out.print("* ");
@@ -11,6 +13,7 @@ public class Kpattern {
               System.out.println();
           }
 
+           //For Second Half Printing.....
         for(int i=2;i<=n;i++) {
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
